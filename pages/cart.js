555244a781d1c905a7e9aebecaa24e5ed114ee57
Kirby <kirbyhackett@hotmail.com>
2019-06-19 16:57:35 -0300

    done orders and cleaned card

--- pages/cart.js
+++ pages/cart.js
@@ -11,42 +11,32 @@ class Cart extends React.Component {
    constructor(...args) {
     super(...args);
      this.state = {
-       
        removeFromCartModal: false
-       
     };
+
     this.removeFromCartToggle = this.removeFromCartToggle.bind(this);
    }
 
    removeFromCartToggle() {
-
     this.setState(prevState => ({
       removeFromCartModal: !prevState.removeFromCartModal
-    }))
+    }));
    }
 
-   handleRemoveFromCartCancel = (e) => {
+  handleRemoveFromCartCancel = (e) => {
     e.preventDefault();
     e.stopPropagation();
-
-
-    this.setState({...this.state, removeFromCartModal:false})
-   }
-
-    handleRemoveFromCartConfirm = (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-
-
     this.setState({...this.state, removeFromCartModal:false});
+  }
 
-    toast.success("The request has been removed from your Cart");
-   }
-
+  handleRemoveFromCartConfirm = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    this.setState({...this.state, removeFromCartModal:false});
+    toast.success("The item has been removed from your cart");
+  }
 
   render() {
-    /* Define variables here */
-    //const { username, password } = this.state;
     const closeRemoveFromCart = <button className="close" onClick={this.removeFromCartToggle}>&times;</button>;
 
     return (
@@ -106,18 +96,14 @@ class Cart extends React.Component {
                     <h3>Cart</h3>
                   </NavItem>
                   <NavItem className="ml-auto">
-
-                  <Button type="button" onClick={()=>this.setState({removeFromCartModal:true})} className="float-left btn-danger delete">Remove</Button>&nbsp;
+                  <Button type="button" onClick={()=>this.setState({removeFromCartModal:true})} className="float-left btn-danger delete">Remove from Cart</Button>&nbsp;
                   <Link href="/checkout">
-
-                  <Button
+                    <Button
                       color="success"
                       onClick={() => this.setState({ addServiceModal: true })}>
-                     Checkout
-                    </Button> 
-
-                    </Link>
-                     
+                       Proceed to checkout
+                    </Button>
+                  </Link>
                   </NavItem>
                 </Nav>
 
@@ -126,43 +112,29 @@ class Cart extends React.Component {
                     <Col s={12} lg={6}>
                       {/* Service name and service picture */}
                       <FormGroup className="mb-8">
-                        <Label className="text-muted">Service Name: Larry&apos;s Landscaping </Label>
-                        
-                       
+                        <Label className="text-muted">Service Name:</Label>
+                        <p>Larry&apos;s Landscaping</p>
                       </FormGroup>
-                      <div className="serviceImage backgroundImage mb-8">
-                      
-                      </div>
+                      <div className="serviceImage backgroundImage mb-8"></div>
                       <Progress value={75} />
-                      <p> Rating: 4 out of 5 </p>
+                      <p>Rating: 4 out of 5</p>
                     </Col>
 
                     <Col s={12} lg={6}>
                       {/* Price of Service */}
-                      <Label className="text-muted">Service Price: </Label>
-                      
-                         <Label className="text-muted">&nbsp; $100.00</Label>
-                         <p>  <Label className="text-muted">Ordered Date: </Label>
-                      
-                         <Label className="text-muted">&nbsp; 2019-06-20 </Label> </p>
-                        
-                        
+                      <Label className="text-muted">Service Price:</Label>
+                      <p>$100.00</p>
+                      <Label className="text-muted">Ordered Date:</Label>
+                      <p>2019-06-20 </p>
 
                       {/* Description of Service */}
-                      <p> </p>
-                      
-                        <Label className="text-muted mt-16">Service Description</Label>
-
-                         <CardText className="mb-16">Keeping landscape healthy, clean, safe and attractive for homes, schools, hotels, offices, etc</CardText>
-
-
-
-                         <h6 className="mt-16"> Reviews: </h6>
-                         <Container> <Row> Great Service! </Row> <Row>Would use this service again... </Row> </Container>
-                      
-
+                      <Label className="text-muted">Service Description:</Label>
+                      <p className="mb-16">Keeping landscape healthy, clean, safe and attractive for homes, schools, hotels, offices, etc</p>
+                      <hr />
+                      <Label className="mt-8 text-muted">Reviews:</Label><br/>
+                      <strong className="mb-0">4/5</strong>
+                      <p>Would use again!</p><br/>
                     </Col>
-
                   </Row>
                 </Container>
               </Form>
@@ -170,34 +142,33 @@ class Cart extends React.Component {
           </Row>
         </Container>
 
-<Modal
-  isOpen={this.state.removeFromCartModal}
-    toggle={this.removeFromCartToggle}>
-    <ModalHeader
-      toggle={this.removeFromCartToggle}
-      close={closeRemoveFromCart}>
-      Remove from Cart
-      </ModalHeader>
-      <ModalBody>
-        <p>Are you sure you want to remove from cart? </p>
-
-      </ModalBody>
-      <ModalFooter>
-        <Button
-        color="secondary" 
-        type="button"
-        onClick={this.handleRemoveFromCartCancel.bind(this)}>Cancel</Button>
-        <Button
-        color="danger"
-        type="button"
-        onClick={this.handleRemoveFromCartConfirm.bind(this)}>
-        Remove
-        </Button>
-
-      </ModalFooter>
-      </Modal>
-      <ToastContainer autoClose={5000} />
-
+        <Modal
+          isOpen={this.state.removeFromCartModal}
+          toggle={this.removeFromCartToggle}>
+          <ModalHeader
+            toggle={this.removeFromCartToggle}
+            close={closeRemoveFromCart}>
+            Remove from Cart
+          </ModalHeader>
+          <ModalBody>
+            <p>Are you sure you want to remove this service from the cart? </p>
+          </ModalBody>
+          <ModalFooter>
+            <Button
+              color="secondary"
+              type="button"
+              onClick={this.handleRemoveFromCartCancel.bind(this)}>
+              Cancel
+            </Button>
+            <Button
+              color="danger"
+              type="button"
+              onClick={this.handleRemoveFromCartConfirm.bind(this)}>
+              Remove Service
+            </Button>
+          </ModalFooter>
+        </Modal>
+        <ToastContainer autoClose={5000} />
       </DefaultLayout>
     );
   }
--- pages/orders.js
+++ pages/orders.js
@@ -1,54 +1,187 @@
 /* Login Page */
 import DefaultLayout from '../layouts/default';
 
+import { ToastContainer, toast } from 'react-toastify';
+import '../node_modules/react-toastify/dist/ReactToastify.css';
 import Link from 'next/link';
-import { Table } from 'reactstrap';
+import { Container, Modal, ModalHeader, ModalBody, ModalTitle, ModalFooter, Label, ButtonGroup, Table, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem, Button } from 'reactstrap';
 
 class Orders extends React.Component {
 
+  constructor(...args) {
+    super(...args);
+    this.state = {
+      rateServiceModal: false,
+     };
+
+     this.rateServiceToggle = this.rateServiceToggle.bind(this);
+  }
+
+  rateServiceToggle() {
+    this.setState(prevState => ({
+      rateServiceModal: !prevState.rateServiceModal
+    }));
+  }
+
+  onChangeOption(e) {
+    if(e.target.value) {
+      e.currentTarget.childNodes[0].innerHTML = e.target.value;
+      //should do better at tracking this
+    }
+  }
+
+  handleSaveChanges() {
+    toast.success("Changes have been successfully saved!");
+  }
+
+  handleRateServiceFormConfirm(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    this.setState({
+      ...this.state,
+      rateServiceModal: false,
+    });
+
+    toast.success("The service has been deleted!");
+
+  }
+
+  confirmRating(e) {
+    this.setState({
+      ...this.state,
+      rateServiceModal: false,
+    });
+
+    console.dir(e.target.innerText);
+
+    toast.success("Your rating of "+ e.target.innerText +" out of 5 has been recorded!");
+  }
+
   render() {
+    const closeRateServiceButton = <button className="close" onClick={this.rateServiceToggle}>&times;</button>;
     /* Wrapping the form with a navigation and footer */
     return (
       <DefaultLayout>
-        <div className="container">
+        <Container>
           {/* Page Title */}
           <h3 className="mt-16 loginFormTitle">Your Order History</h3>
           {/* Table */}
           <Table bordered responsive hover className="mt-8">
             <thead>
               <tr>
-                <th>Order ID</th>
-                <th>Order Description</th>
+                <th>Date</th>
                 <th>Vendor</th>
-                <th>Order Status</th>
-                <th>Order Date</th>
+                <th>Description</th>
+                <th>Status</th>
+                <th>Add Rating</th>
               </tr>
             </thead>
             <tbody>
               <tr>
-                <th scope="row">1</th>
-                <td>Create an additional power point.</td>
+                <th scope="row">2019-06-13</th>
                 <td>Oskof Electricals</td>
-                <td>In-Progress</td>
-                <td>2019-06-13</td>
+                <td>Create an additional power point.</td>
+                <td>
+                  <UncontrolledDropdown onClick={this.onChangeOption.bind(this)}>
+                    <DropdownToggle caret color="light">
+                      In-progress
+                    </DropdownToggle>
+                    <DropdownMenu>
+                      <DropdownItem value="In-progress">In-progress</DropdownItem>
+                      <DropdownItem value="Pending">Pending</DropdownItem>
+                      <DropdownItem value="Completed">Completed</DropdownItem>
+                    </DropdownMenu>
+                  </UncontrolledDropdown>
+                </td>
+                <td>
+                  <Button
+                    color="primary"
+                    onClick={() => this.setState({ rateServiceModal: true })}>
+                    Review Service
+                  </Button>
+                </td>
               </tr>
               <tr>
-                <th scope="row">2</th>
-                <td>Conference room and chairs</td>
-                <td>Best Furnitures</td>
-                <td>Closed</td>
-                <td>2019-05-23</td>
+                <th scope="row">2019-06-13</th>
+                <td>Oskof Electricals</td>
+                <td>Create an additional power point.</td>
+                <td>
+                  <UncontrolledDropdown onClick={this.onChangeOption.bind(this)}>
+                    <DropdownToggle caret color="light">
+                      In-progress
+                    </DropdownToggle>
+                    <DropdownMenu>
+                      <DropdownItem value="In-progress">In-progress</DropdownItem>
+                      <DropdownItem value="Pending">Pending</DropdownItem>
+                      <DropdownItem value="Completed">Completed</DropdownItem>
+                    </DropdownMenu>
+                  </UncontrolledDropdown>
+                </td>
+                <td>
+                  <Button
+                    color="primary"
+                    onClick={() => this.setState({ rateServiceModal: true })}>
+                    Review Service
+                  </Button>
+                </td>
               </tr>
               <tr>
-                <th scope="row">3</th>
-                <td>General Landscaping</td>
-                <td>Felixco Lawn Care</td>
-                <td>Pending Approval</td>
-                <td>2019-03-25</td>
+                <th scope="row">2019-06-13</th>
+                <td>Oskof Electricals</td>
+                <td>Create an additional power point.</td>
+                <td>
+                  <UncontrolledDropdown onClick={this.onChangeOption.bind(this)}>
+                    <DropdownToggle caret color="light">
+                      In-progress
+                    </DropdownToggle>
+                    <DropdownMenu>
+                      <DropdownItem value="In-progress">In-progress</DropdownItem>
+                      <DropdownItem value="Pending">Pending</DropdownItem>
+                      <DropdownItem value="Completed">Completed</DropdownItem>
+                    </DropdownMenu>
+                  </UncontrolledDropdown>
+                </td>
+                <td>
+                  <Button
+                    color="primary"
+                    onClick={() => this.setState({ rateServiceModal: true })}>
+                    Review Service
+                  </Button>
+                </td>
               </tr>
             </tbody>
           </Table>
-        </div>
+
+          <Button
+            color="success"
+            className="float-right"
+            onClick={this.handleSaveChanges}>
+            Save Changes
+          </Button>
+        </Container>
+
+        {/* Delete Service Modal */}
+        <Modal
+          isOpen={this.state.rateServiceModal}
+          toggle={this.rateServiceToggle}>
+          <ModalHeader
+            toggle={this.deleteServiceToggle}
+            close={closeRateServiceButton}>
+            Rate Service
+          </ModalHeader>
+          <ModalBody className="orderModalBody">
+            <Label className="mt-8">How would you rate the service out of 5?</Label>
+            <ButtonGroup size="lg">
+              <Button color="light" onClick={this.confirmRating.bind(this)}>1</Button>
+              <Button color="light" onClick={this.confirmRating.bind(this)}>2</Button>
+              <Button color="light" onClick={this.confirmRating.bind(this)}>3</Button>
+              <Button color="light" onClick={this.confirmRating.bind(this)}>4</Button>
+              <Button color="light" onClick={this.confirmRating.bind(this)}>5</Button>
+            </ButtonGroup>
+          </ModalBody>
+        </Modal>
+
+        <ToastContainer autoClose={5000}/>
       </DefaultLayout>
     );
   }
